df09f4383048fc0a60a30df26bdca46c569c63bb
BrandonVA <codebva@gmail.com>
2021-01-31 19:52:05 -0800

    Added class to make text center.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,10 @@ function Footer() {
   const styles = {
     width: "100%",
     height: "60px",
+    color: "white",
   };
   return (
-    <footer className="footer mt-auto" style={styles}>
+    <footer className="footer mt-auto text-center" style={styles}>
       <p>&#169; Brandon VanAllen 2021</p>
     </footer>
   );
