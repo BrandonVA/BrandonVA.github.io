0b27778c966d6dde9c639599bb9091b042129707
BrandonVA <codebva@gmail.com>
2021-02-02 12:57:23 -0800

    Added logo in the navbar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,8 +9,19 @@ function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg" style={{ background: "#020202" }}>
-      <Link className="navbar-brand" to="/">
-        Brandon VanAllen
+      <Link className="navbar-brand" to="/" style={{ fontSize: "1.6rem" }}>
+        <img
+          style={{
+            width: "1.5rem",
+            height: "1.5rem",
+            marginRight: "5px",
+            paddingBottom: "2px",
+          }}
+          src={`${process.env.PUBLIC_URL}/favicon_io/favicon-32x32.png`}
+          alt=""
+        />
+
+        <span>Brandon VanAllen</span>
       </Link>
       <div className="ml-auto">
         <ul className="navbar-nav">
