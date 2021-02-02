f8d90cac68f7e99a07eabd5e4cbe7356718e00a2
BrandonVA <codebva@gmail.com>
2021-02-02 08:01:13 -0800

    Added links to github and linkin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 import "./style.css";
 
 function Footer() {
@@ -7,9 +12,34 @@ function Footer() {
     height: "60px",
     color: "white",
   };
+
   return (
-    <footer className="footer mt-auto text-center" style={styles}>
-      <p>&#169; Brandon VanAllen 2021</p>
+    <footer className=" container-fluid py-3 footer">
+      <div className="row">
+        <div className="col-lg-5"></div>
+        <div className="col-lg-2">
+          <div className="center-block center-sm ">
+            <a
+              href="https://www.linkedin.com/in/brandon-vanallen-057419130/"
+              target="_blank"
+            >
+              <i className="fab fa-linkedin footer-icon"></i>
+              <FontAwesomeIcon icon={faLinkedin} className="footerIcon" />
+              Brandon VanAllen
+            </a>
+          </div>
+          <div className="center-block center-sm ">
+            <a href="https://github.com/BrandonVA" target="_blank">
+              <FontAwesomeIcon icon={faGithub} className="footerIcon" />
+              BrandonVA
+            </a>
+          </div>
+          <div className="center-block center-sm ">
+            <p style={{ marginBottom: 0 }}>&#169; Brandon VanAllen</p>
+          </div>
+        </div>
+        <div className="col-lg-5"></div>
+      </div>
     </footer>
   );
 }
