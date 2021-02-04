488c500b75109015d773bdfd55cbc8d9a61ef056
BrandonVA <codebva@gmail.com>
2021-02-03 16:53:22 -0800

    changed picture and moved elements.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,31 +5,41 @@ import Col from "react-bootstrap/Col";
 const Home = () => {
   const styles = {
     colImage: {
-      width: "35%",
+      maxWidth: "350px",
       height: "auto",
-      minWidth: "300px",
-      display: "flex",
-      justifyContent: "space-between",
     },
   };
   window.document.title = "Brandon VanAllen | Home";
   return (
-    <>
-      <Row>
-        <Col className="text-center">
-          <div>
-            <h1 className="">Hello I'm Brandon VanAllen</h1>
+    <div style={{ minHeight: "100vh" }}>
+      <Row className="mt-5">
+        <Col className="mt-5 p-3" lg={6} sm={12}>
+          {/* <h1 className="mb-5">Hello I'm Brandon VanAllen</h1> */}
+          {/* <h3 className="mb-4"> Full Stack Web Developer</h3> */}
+          <div className=" sm-center">
             <img
               style={styles.colImage}
-              className="text-center mx-auto profile-pic my-5"
-              src={`${process.env.PUBLIC_URL}/images/ProfileImg.jpg`}
+              className="profile-pic rounded img-fluid shadow"
+              src={`${process.env.PUBLIC_URL}/images/Portfolio-pic.jpg`}
               alt="profile-pic"
             />
-            <h3 className=""> Full Stack Web Developer</h3>
+          </div>
+        </Col>
+        <Col className="mb-5 p-3" lg={6} sm={12}>
+          <div>
+            <h3 className="mb-4 sm-center">About Me</h3>
+            <p className="about-me">
+              Hi I'm Brandon VanAllen I'm a Full Stack Web Developer. I recently
+              graduated from UW Full Stack Coding Bootcamp. I'm passionate about
+              building applications and enjoy solving the problems that one
+              would encounter developing applications. I'm excited to change my
+              career into something that is challenging and has a lot of room to
+              grow.
+            </p>
           </div>
         </Col>
       </Row>
-    </>
+    </div>
   );
 };
 
