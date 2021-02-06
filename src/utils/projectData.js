9474f8b5a1c40228008e449644386a9682480e72
BrandonVA <codebva@gmail.com>
2021-02-05 16:59:39 -0800

    Added a desc for each project.

--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -4,48 +4,62 @@ const projectData = [
     deployedUrl: "https://carolinayo.github.io/the-gifd-of-food/",
     repoUrl: "https://github.com/CarolinaYo/the-gifd-of-food",
     image: "the-gifd-of-food.png",
+    desc:
+      "A simple site to get some recipes and gives you some gif's to go along with your recipes. ",
   },
   {
     title: "Weather Forecast",
     deployedUrl: "https://brandonva.github.io/Weather-Forecast/",
     repoUrl: "https://github.com/BrandonVA/Weather-Forecast",
     image: "Weather-forecast.png",
+    desc:
+      "This is a simple weather app that gets the weather for a given city and displays a 5 day forecast.",
   },
   {
     title: "Note Taker",
     deployedUrl: "https://still-castle-69913.herokuapp.com/",
     repoUrl: "https://github.com/BrandonVA/Note-Taker",
     image: "note-taker.png",
+    desc: "Basic not taking app that uses a json file for a database",
   },
   {
     title: "Burger App",
     deployedUrl: "https://floating-taiga-94665.herokuapp.com/",
     repoUrl: "https://github.com/BrandonVA/Burger-App",
     image: "Burger-App.png",
+    desc:
+      "This app lets your keep track of what burgers you like to devour. it uses a MySQL database.",
   },
   {
     title: "Naughty or Nice",
     deployedUrl: "https://evening-springs-93825.herokuapp.com/",
     repoUrl: "https://github.com/NirmalaAbothu/Naughty_or_Nice",
     image: "NorN.png",
+    desc:
+      "This is Christmas list tracker that uses a MongoDB and Passport for user Authentication",
   },
   {
     title: "Fitness Tracker",
     deployedUrl: "https://enigmatic-escarpment-34858.herokuapp.com/",
     repoUrl: "https://github.com/BrandonVA/Fitness-Tracker",
     image: "fitness-tracker.png",
+    desc: "This is a site that keeps track of a users workouts using a MongoDB",
   },
   {
     title: "Budget Tracker",
     deployedUrl: "https://shielded-escarpment-32907.herokuapp.com/",
     repoUrl: "https://github.com/BrandonVA/Budget-Tracker",
     image: "budget-tracker.png",
+    desc:
+      "This is a Progressive Web App that can be used offline it tracks a users budget.",
   },
   {
     title: "Employee Directory",
     deployedUrl: "https://github.com/BrandonVA/Employee-Directory",
     repoUrl: "https://brandonva.github.io/Employee-Directory/",
     image: "employee-directory.png",
+    desc:
+      "This is a user directory made with react where a user can filter or sort employees by their desired field.",
   },
 ];
 
