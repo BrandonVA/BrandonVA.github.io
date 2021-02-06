0cbfd8f6e8d1e2def4bf3ed0f4d25882294f2fa9
BrandonVA <codebva@gmail.com>
2021-02-05 16:59:16 -0800

    Added the new project heading component.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,12 +3,14 @@ import Container from "react-bootstrap/Container";
 import ProjectRow from "../components/ProjectRow/index";
 import ProjectsContext from "../utils/ProjectsContext";
 import projects from "../utils/projectData";
+import ProjectHeading from "../components/ProjectHeading/ProjectHeading";
 
 const Portfolio = () => {
   window.document.title = "Brandon VanAllen | Portfolio";
   return (
     <div>
       <ProjectsContext.Provider value={projects}>
+        <ProjectHeading />
         <Container>
           <ProjectRow />
         </Container>
